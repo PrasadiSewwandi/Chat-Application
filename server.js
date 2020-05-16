8ed47e95f344160e2cc87331602105ea580b13a2
Prasadi Sewwandi <prasadi19943@gmail.com>
2020-05-16 22:21:15 +0530

    done

--- server.js
+++ server.js
@@ -1,34 +1,69 @@
 var express = require('express');
-var mongoose = require('mongoose');
-var bodyParser = require('body-parser');
-var http = require('http').Server(app);
-var io = require('socket.io') (http);
+var bodyParser = require('body-parser')
 var app = express();
-var server = app.listen(3000,() =>{
-    console.log('server is running on port', server.address().port);
-});
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var mongoose = require('mongoose');
+
 app.use(express.static(__dirname));
-var dbUrl = 'mongodb+srv://cluster0-0basb.mongodb.net/chatApplicationDB'
-mongoose.connect(dbUrl , (err) => {
-    console.log('mongodb connected', err);
-});
-var Message = mongoose.model('Message',{name : String, message : String})
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-app.get('/messages',(req,res) =>{
-    Message.find({},(err, message)=> {
-        res.send(message);
-    })
-});
-app.post('/messages',(req,res) => {
-    var message = new Message(req.body);
-    message.save((err) => {
-        if(err)
-        sendStatus(500);
-        io.emit('message', req.body);
-        res.sendStatus(200);
-    })
-});
-io.on('connection', ()=>{
-    console.log('a user isconnected')
+app.use(bodyParser.urlencoded({extended: false}))
+
+var Message = mongoose.model('Message',{
+  name : String,
+  message : String
 })
+
+var dbUrl = 'mongodb+srv://prasadi:mongo@cluster0-0basb.mongodb.net/chatApplicationDB';
+app.get('/messages', (req, res) => {
+  Message.find({},(err, messages)=> {
+    res.send(messages);
+  })
+})
+
+
+app.get('/messages/:user', (req, res) => {
+  var user = req.params.user
+  Message.find({name: user},(err, messages)=> {
+    res.send(messages);
+  })
+})
+
+
+app.post('/messages', async (req, res) => {
+  try{
+    var message = new Message(req.body);
+
+    var savedMessage = await message.save()
+      console.log('saved');
+
+    var censored = await Message.findOne({message:'badword'});
+      if(censored)
+        await Message.remove({_id: censored.id})
+      else
+        io.emit('message', req.body);
+      res.sendStatus(200);
+  }
+  catch (error){
+    res.sendStatus(500);
+    return console.log('error',error);
+  }
+  finally{
+    console.log('Message Posted')
+  }
+
+})
+
+
+
+io.on('connection', () =>{
+  console.log('a user is connected')
+})
+
+mongoose.connect(dbUrl ,{ username: 'prasadi', password: 'mongo'} ,(err) => {
+  console.log('mongodb connected',err);
+})
+
+var server = http.listen(3000, () => {
+  console.log('server is running on port', server.address().port);
+});
